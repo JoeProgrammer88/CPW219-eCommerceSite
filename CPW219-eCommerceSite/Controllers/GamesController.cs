cc3c376e7ebc42d2fb95f13e1841c3bc04e3f277
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-02-26 15:29:36 -0800

    Refactored game catalog to use a ViewModel

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -21,13 +21,18 @@ namespace CPW219_eCommerceSite.Controllers
 
             int currPage = id ?? 1; // Set currPage to id if it has a value, otherwise use 1
 
+            int totalNumOfProducts = await _context.Games.CountAsync();
+            double maxNumPages = Math.Ceiling((double)totalNumOfProducts / NumGamesToDisplayPerPage);
+            int lastPage = Convert.ToInt32(maxNumPages); // Rounding pages up, to next whole page number
+
             List<Game> games = await (from game in _context.Games
                                      select game)
                                      .Skip(NumGamesToDisplayPerPage * (currPage - PageOffset))
                                      .Take(NumGamesToDisplayPerPage)
                                      .ToListAsync();
 
-            return View(games);
+            GameCatalogViewModel catalogModel = new(games, lastPage, currPage);
+            return View(catalogModel);
         }
 
         [HttpGet]
